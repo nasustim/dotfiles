1cb9d3ce40def64cceef98cb4e0b6af37ed0dc6d
nasustim <mitsuhibino@gmail.com>
2022-09-15 19:28:32 +0900

    add .gitconfig

--- git/setup.sh
+++ git/setup.sh
@@ -6,3 +6,10 @@ else
   echo "add symbolic link .gitignore_global"
   ln -fsv "${PWD}/git/.gitignore_global" "${HOME}/.gitignore_global"
 fi
+
+if [ -L "${HOME}/.gitconfig" ]; then
+  echo "skip link .gitconfig"
+else
+  echo "add symbolic link .gitconfig"
+  ln -fsv "${PWD}/git/.gitconfig" "${HOME}/.gitconfig"
+fi
