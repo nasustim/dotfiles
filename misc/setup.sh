fb013a6bcb44cf54398441e6f475aa6d25baf952
nasustim <mitsuhibino@gmail.com>
2023-06-23 23:59:29 +0900

    fix: remove Brewfile

--- misc/setup.sh
+++ misc/setup.sh
@@ -10,14 +10,3 @@ else
   ln -fsv "${PWD}/${TYPE}/.cargo/env" "${HOME}/.cargo/env"
 fi
 
-if [ "$(uname)" == 'Darwin' ]; then
-  if [ -L "${HOME}/.Brewfile" ]; then
-    echo "skip link .Brewfile"
-  else
-    echo "add symbolic link .Brewfile"
-    ln -fsv "${PWD}/${TYPE}/macos/.Brewfile" "${HOME}/.Brewfile"
-  fi
-
-  echo "prepare Brewfile"
-  echo "you have to execute '$ brew bundle --file ~/.Brewfile'"
-fi
