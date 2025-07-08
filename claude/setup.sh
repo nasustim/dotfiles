1535a46bc138fad5b2c328d9dbcd6c460d20ac42
nasustim <mitsuhibino@gmail.com>
2025-07-08 12:37:07 +0900

    Fix

--- claude/setup.sh
+++ claude/setup.sh
@@ -4,6 +4,6 @@ if [ -L "${HOME}/.claude" ]; then
   echo "skip link .claude"
 else
   echo "add symbolic link .claude"
-  ln -fsv "${PWD}/claude/.claude/CLAUDE.json" "${HOME}/.claude/CLAUDE.json"
+  ln -fsv "${PWD}/claude/.claude/CLAUDE.md" "${HOME}/.claude/CLAUDE.md"
   ln -fsv "${PWD}/claude/.claude/settings.json" "${HOME}/.claude/settings.json"
 fi
